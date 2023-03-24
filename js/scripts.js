1e654193d606a330d89c2cc1d6bdb11754bbba2e
Max Betich <betichm@gmail.com>
2023-03-24 14:11:10 -0700

    add hide/reveal functionality to images

--- js/scripts.js
+++ js/scripts.js
@@ -6,19 +6,32 @@ function languageSelector(event) {
   const question4 = document.querySelector("input[name='question4']:checked").value;
   const question5 = document.querySelector("input[name='question5']:checked").value;
 
+  document.getElementById("img1").setAttribute("class", "hidden");
+  document.getElementById("img2").setAttribute("class", "hidden");
+  document.getElementById("img3").setAttribute("class", "hidden");
+  document.getElementById("img4").setAttribute("class", "hidden");
+  document.getElementById("img5").setAttribute("class", "hidden");
+  document.getElementById("img6").setAttribute("class", "hidden");
+
   let output;
   if (question1  === "yes") {
     output = "JavaScript is the language for you!";
+    document.getElementById("img1").removeAttribute("class");
   } else if (question2 === "yes") {
     output = "You'll likely find Ruby to be right up your alley!";
+    document.getElementById("img2").removeAttribute("class");
   } else if (question3 === "yes") {
     output = "Python is a heck of a good time for someone as cool as you!";
+    document.getElementById("img3").removeAttribute("class");
   } else if (question4 === "yes") {
     output = "Go is the language for programmers on the move!";
+    document.getElementById("img4").removeAttribute("class");
   } else if (question5 === "yes") {
     output = "C# will let you code with perfect pitch!";
+    document.getElementById("img5").removeAttribute("class");
   } else {
-    output = "The only logical choice for a first language is Swift!"
+    output = "The only logical choice for a first language is Swift!";
+    document.getElementById("img6").removeAttribute("class");
   }
 
   document.getElementById("output").innerText = output
